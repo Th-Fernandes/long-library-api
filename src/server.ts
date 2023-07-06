905d67a3906c8b5439e97883472392629da0a467
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-07-06 09:57:50 -0300

    feat: express server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,31 @@
-import http from 'http';
+// import http from 'http';
 
-const port = 8080;
+// const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
+// const routes = {
+//   '/': 'this still is a webpage :/',
+//   '/books': 'there is a lot of books here'
+// }
 
-  res.end('this is a webpage :)')
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain'
+//   })
+
+//   res.end(routes[req.url])
+// })
+
+// server.listen(port, () => console.log(`Listening on: http://localhost:${port}`))
+
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3333; 
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 200 })
 })
 
-server.listen(port, () => console.log(`Listening on: http://localhost:${port}`))
+app.listen(port, () => console.log(`API entry: http://localhost:${port}`))
